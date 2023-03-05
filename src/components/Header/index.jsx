a4c477a6df24fd1ac6b932a2d8b25a9fa066baf2
jean carlos <matos.sd52@gmail.com>
2023-03-05 00:07:29 -0300

    ajustes de estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,11 @@ import "./styles.css";
 export default function Header() {
   return (
     <>
+
       <div className="container-header">
         <button className="btn-logout">Sair</button>
       </div>
+            <div className="border-top-header"></div>
     </>
   );
 }
