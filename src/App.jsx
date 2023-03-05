ad610b08ec240c78cc30104fbd23c1f143f034d4
jean carlos <matos.sd52@gmail.com>
2023-03-04 22:16:12 -0300

    resolvendo bug rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import './App.css'
-import Header from './components/Header'
 import SignIn from './pages/Sign-In'
 import SignUp from './pages/Sign-up';
-import Main from './pages/Main';
-import { Routes, Route, BrowserRouter } from 'react-router-dom'; 
+import Content from './pages/Content';
+import { Routes, Route} from 'react-router-dom'; 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
       <Routes>
          <Route path="/">
           <Route path="/" element={<SignIn/>}/>
           <Route path="/sign-in" element={<SignIn/>}/>
         </Route>
           <Route path="/sign-up" element={<SignUp/>}/>
-          <Route path="/main" element={<Main/>}/>
+          <Route path="/main" element={<Content/>}/>
       </Routes>
-      </BrowserRouter>
     </div>
   )
 }
